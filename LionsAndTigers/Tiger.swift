4fbfe2838f2b9c68debd6dc5e4587cdd9cf1bf7d
Antonio Caravaca Vega <tony@iMac24.local>
2014-11-20 13:33:58 +0100

    First commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Antonio Caravaca Vega on 3/11/14.
+//  Copyright (c) 2014 ACV. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var name = ""
+    var age = 0
+    var breed = ""
+    var image = UIImage(named: "")
+}
